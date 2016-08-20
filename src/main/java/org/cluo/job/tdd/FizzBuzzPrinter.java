a97cc025ada3c2b86113e649515f9cdf36c76527
Charles Luo <charles.luo@first-utility.com>
2016-08-20 13:25:36 +0100

    can replace multiples of 15 with fizzbuzz

--- src/main/java/org/cluo/job/tdd/FizzBuzzPrinter.java
+++ src/main/java/org/cluo/job/tdd/FizzBuzzPrinter.java
@@ -10,17 +10,35 @@ public class FizzBuzzPrinter {
         final int numberCount=endNumber - startNumber + 1;
         final List<String> numbers = new ArrayList<String>(numberCount);
         for (int i = 0; i < numberCount; i++) {
-            int entryItem = startNumber;
-            if (entryItem == 3){
-                numbers.add("fizz");
-            }else if (entryItem==5) {
-                numbers.add("buzz");
-            } else {
-                numbers.add(String.valueOf(entryItem));
-            }
+            populateFizzbuzzNumberCollection(numbers, startNumber);
             startNumber++;
         }
         return numbers;
+    }
+
+    private void populateFizzbuzzNumberCollection(final List<String> collection, final int entryItem) {
+        if (isMultipleOf15(entryItem)){
+           collection.add("fizzbuzz");
+        } else if (isMultipleOf3(entryItem)){
+           collection.add("fizz");
+        } else if (isMultipleOf5(entryItem)) {
+           collection.add("buzz");
+        }
+        else {
+            collection.add(String.valueOf(entryItem));
+        }
+    }
+
+    private boolean isMultipleOf5(final int entryItem) {
+        return entryItem % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(final int entryItem) {
+        return entryItem % 3 == 0;
+    }
+
+    private boolean isMultipleOf15(int entryItem) {
+        return entryItem % 15 == 0;
     }
 
     private void validateArguments(int startNumber, int endNumber) {
--- src/test/java/org/cluo/job/tdd/FizzBuzzPrinterTest.java
+++ src/test/java/org/cluo/job/tdd/FizzBuzzPrinterTest.java
@@ -1,6 +1,5 @@
 package org.cluo.job.tdd;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Before;
@@ -53,25 +52,56 @@ public class FizzBuzzPrinterTest {
 
    @Test
    public void print_OutputNumbersFrom1To2() throws Exception {
-      final List<String> expectedNumberRange = Arrays.asList(new String[]{"1","2"});
+      final List<String> expectedNumberRange = Arrays.asList("1","2");
       final List<String> actualNumberRange = fizzBuzzPrinter.print(1, 2);
       assertEquals("Printed numbers are not expected", actualNumberRange, expectedNumberRange);
    }
    
    @Test
-   public void print_number3replacedByFizzInNumbers1To3() throws Exception {
+   public void print_number3replacedByFizz_InNumbers1To3() throws Exception {
         final List<String> numbers = fizzBuzzPrinter.print(1, 3);
         assertThat("Output should not contain 3",numbers, not(hasItem("3")));
         assertThat("Output should contain fizz",numbers, hasItem("fizz"));
    }
 
+    @Test
+    public void print_number5replacedByBuzz_InNumbers1To5() throws Exception {
+        final List<String> numbers = fizzBuzzPrinter.print(1, 5);
+        assertThat("Output should not contain 5", numbers, not(hasItem("5")));
+        assertThat("Output should contain buzz", numbers, hasItem("buzz"));
+    }
+   
+    @Test
+    public void print_number10replacedByBuzz_InNumbers1To10() throws Exception {
+        final List<String> numbers = fizzBuzzPrinter.print(1, 10);
+        assertThat("Output should not contain 10", numbers, not(hasItem("10")));
+        assertThat("Output should contain buzz", numbers, hasItem("buzz"));
+    }
+
    @Test
-     public void print_number5replacedByBuzzInNumbers1To5() throws Exception {
-          final List<String> numbers = fizzBuzzPrinter.print(1, 5);
-          assertThat("Output should not contain 5",numbers, not(hasItem("5")));
-          assertThat("Output should contain buzz",numbers, hasItem("buzz"));
+    public void print_number6replacedByFizz_InNumbers1To6() throws Exception {
+         final List<String> numbers = fizzBuzzPrinter.print(1, 6);
+         assertThat("Output should not contain 6",numbers, not(hasItem("6")));
+         assertThat("Output should contain fizz",numbers, hasItem("fizz"));
+    }
+
+   @Test
+     public void print_number9And12replacedByFizz_15replacedByBuzz_InNumbers1To15() throws Exception {
+        final List<String> numbers = fizzBuzzPrinter.print(1, 15);
+        assertThat("Output should not contain 9", numbers, not(hasItem("9")));
+        assertThat("Output should not contain 12", numbers, not(hasItem("12")));
+        assertThat("Output should contain fizz", numbers, hasItem("fizz"));
+
+        assertThat("Output should not contain 15", numbers, not(hasItem("15")));
+        assertThat("Output should contain buzz", numbers, hasItem("buzz"));
      }
 
+    @Test
+    public void print_number15replacedByFizzbuzz_InNumbers1To15() throws Exception {
+        final List<String> numbers = fizzBuzzPrinter.print(1, 15);
+        assertThat("Output should not contain 15", numbers, not(hasItem("15")));
+        assertThat("Output should contain fizzbuzz", numbers, hasItem("fizzbuzz"));
+    }
 
    private void expectedExceptionDetails(Class<? extends Throwable> classType, String expectedExceptionMessage){
       expectedException.expect(classType);
