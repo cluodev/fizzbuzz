f7b78002b5ca8f6593646a3a4924e397baf37bdd
Charles Luo <charles.luo@first-utility.com>
2016-08-21 00:46:47 +0100

    corrected test as now number contains 3 is replaced with 'lucky'

--- src/test/java/org/cluo/job/tdd/FizzBuzzRunnerTest.java
+++ src/test/java/org/cluo/job/tdd/FizzBuzzRunnerTest.java
@@ -77,7 +77,7 @@ public class FizzBuzzRunnerTest {
     }
 
     private String buildExpectedResults() {
-        return "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz\n";
+        return "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz\n";
     }
 
 }
